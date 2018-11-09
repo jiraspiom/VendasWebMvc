48dff2277e284e2f83dd6a692e13ec142add9aaf
gilmar <jiraspiom@yahoo.com.br>
2018-11-09 16:44:03 -0200

    * HomeController.cs: Redirado e renomiando arquivos basico
    * Index.cshtml:
    * Sobre.cshtml:
    * Contato.cshtml:
    * _Layout.cshtml:
    * Privacidade.cshtml:

--- VendasWebMvc/Controllers/HomeController.cs
+++ VendasWebMvc/Controllers/HomeController.cs
@@ -15,21 +15,21 @@ namespace VendasWebMvc.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Sobre()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Contato()
         {
             ViewData["Message"] = "Your contact page.";
 
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Privacidade()
         {
             return View();
         }
