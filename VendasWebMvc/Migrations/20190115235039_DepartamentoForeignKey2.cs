968b3e3499e000ac6417e59b93b125ae5f61ec71
jiraspiom <jiraspiom@yahoo.com.br>
2019-01-20 11:13:55 -0200

    forekey nao nula (integridade referencial)

---
+++ VendasWebMvc/Migrations/20190115235039_DepartamentoForeignKey2.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VendasWebMvc.Migrations
+{
+    public partial class DepartamentoForeignKey2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vendedor_Departamento_DepartamentoId",
+                table: "Vendedor");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartamentoId",
+                table: "Vendedor",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Vendedor",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vendedor_Departamento_DepartamentoId",
+                table: "Vendedor",
+                column: "DepartamentoId",
+                principalTable: "Departamento",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vendedor_Departamento_DepartamentoId",
+                table: "Vendedor");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Vendedor");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartamentoId",
+                table: "Vendedor",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vendedor_Departamento_DepartamentoId",
+                table: "Vendedor",
+                column: "DepartamentoId",
+                principalTable: "Departamento",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
